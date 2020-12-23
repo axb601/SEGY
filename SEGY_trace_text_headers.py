3d40a4e7c7f61ac12b2967dd4b152205d911ddea
Alex Birch-Hawkins <axb601@gmail.com>
2020-12-23 14:40:07 +0000

    Add files via upload

---
+++ SEGY_trace_text_headers.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 23 14:00:45 2020
+
+@author: BIRCHHAWKINS
+
+Two functions for reading, formatting, and displayed standard SEGY trace and text headers
+
+"""
+
+import segyio 
+import pandas as pd
+import re
+
+
+def parse_trace_headers(segyfile, n_traces):
+    '''
+    Parse the segy file trace headers into a pandas dataframe.
+    Column names are defined from segyio internal tracefield
+    One row per trace
+    '''
+    # Get all header keys
+    headers = segyio.tracefield.keys
+    # Initialize dataframe with trace id as index and headers as columns
+    df = pd.DataFrame(index=range(1, n_traces + 1),
+                      columns=headers.keys())
+    # Fill dataframe with all header values
+    for k, v in headers.items():
+        df[k] = segyfile.attributes(v)[:]
+    return df
+
+
+def parse_text_header(segyfile):
+    '''
+    Format segy text header into a readable, clean dict
+    '''
+    raw_header = segyio.tools.wrap(segyfile.text[0])
+    # Cut on C*int pattern
+    cut_header = re.split(r'C ', raw_header)[1::]
+    # Remove end of line return
+    text_header = [x.replace('\n', ' ') for x in cut_header]
+    text_header[-1] = text_header[-1][:-2]
+    # Format in dict
+    clean_header = {}
+    i = 1
+    for item in text_header:
+        key = "C" + str(i).rjust(2, '0')
+        i += 1
+        clean_header[key] = item
+    return clean_header
